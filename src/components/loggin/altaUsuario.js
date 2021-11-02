b34e594d3411676dceaa6f90cbba5a9599469ced
Lidiafg91 <lidiaflores.gomez91@gmail.com>
2021-11-02 19:30:28 +0100

    inputs alta usuario y axios post en user js

--- src/components/loggin/altaUsuario.js
+++ src/components/loggin/altaUsuario.js
@@ -1,12 +1,16 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 // import axios from "axios";
-// import { añadirContacto } from "../features/contactosSlice";
-// import {  useDispatch } from "react-redux";
-// function AltaUsuario() {
-//   const dispatch = useDispatch(); 
-//   const [nombre, setNombre] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [email, setEmail] = useState("");
+// import { addDataUser } from "../features/userSlice";
+import {  useDispatch } from "react-redux";
+// import servicioPost from "../services/user";
+
+
+
+function AltaUsuario(props) {
+  const dispatch = useDispatch(); 
+  const [nombre, setNombre] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
  
 
   
@@ -14,61 +18,48 @@
 //   const enviarDatos = () => {
 //     const data = {
 //       nombre: nombre,
-//       apellidos: apellidos,
-//       telefono: telefono,
-//       email: email,
-//       direccion: direccion,
+//       password: password, 
+//       email: email,    
 //     };
-//     //comprobar si el contacto ya esta dado de alta, y paRA usariamos
 //     postData(data);
 //   };
 
 //   async function postData(payload = {}) {
-//     dispatch(añadirContacto(payload))
-//     axios
-//       .post("http://127.0.0.1:8081/procesarPost", payload)
-//       .then(function (response) {
-//         console.log(response.data);
-//         setNombre("");
-//         setApellidos("");
-//         setTelefono("");
-//         setEmail("");
-//         setDireccion("");
-//       })
-//       .catch(function (error) {
-//         console.log(error);
-//       });
+//     dispatch(addDataUser(payload))
+
+// //     function addUser(payload)
 //   }
 
-//   return (
-//     <div className="App">
-//       {/* <form method="" action=""> */}
-//       <input
-//         type="text"
-//         value={nombre}
-//         placeholder="Nombre"
-//         name="nombre"
-//         onChange={(e) => setNombre(e.target.value)}
-//       />
-//       <input
-//         type="text"
-//         value={apellidos}
-//         placeholder="Apellidos"
-//         name="apellidos"
-//         onChange={(e) => setApellidos(e.target.value)}
-//       />
-    
-//       <input
-//         type="text"
-//         value={email}
-//         placeholder="Email"
-//         name="email"
-//         onChange={(e) => setEmail(e.target.value)}
-//       />
-     
-//       <button onClick={enviarDatos}>Enviar</button>
-//     </div>
-//   );
-// }
 
-// export default AltaUsuario;
+  return (
+    <div className="App">
+    
+      <input
+        type="text"
+        value={nombre}
+        placeholder="Nombre"
+        name="nombre"
+        onChange={(e) => setNombre(e.target.value)}
+      />
+      <input
+        type="text"
+        value={password}
+        placeholder="Introducir contraseña"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    
+      <input
+        type="text"
+        value={email}
+        placeholder="Email"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+     
+      {/* <button onClick={enviarDatos}>Enviar</button> */}
+    </div>
+  );
+}
+
+export default AltaUsuario;
--- src/services/user.js
+++ src/services/user.js
@@ -6,4 +6,17 @@
 // }
 
 // export default addUser;
+import axios from 'axios';
 
+function addUser(nombre,password,email){
+axios({
+    method: 'post',
+    url: 'http://127.0.0.1:8081/procesarPost',
+    data: {
+        nombre: nombre,
+        password: password, 
+        email: email,
+    }
+  });
+}
+export default addUser;
